96a95651901f6cc832fb2fb6d6a3645604c524f9
Ben Ritcey <ben+github@ritcey.com>
2023-05-17 12:35:47 -0400

    updated code

--- src/main.py
+++ src/main.py
@@ -9,4 +9,4 @@ def get_env_message():
 
 @app.get("/")
 def home_view():
-    return  {"hello": "world", "cron": "smooth-cronjob", "watchtower": "working", "env-message": get_env_message()}
+    return  {"hello": "world", "cron": "smooth-cronjob", "watchtower": "working", "env-message": get_env_message(), "secret-message": get_secret_message()}
